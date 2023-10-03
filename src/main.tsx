8a7c026d8f937005f713b650a157efb1aa63268b
Erick Christian <erickchristian48@gmail.com>
2023-10-03 08:25:55 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import "./index.css";
+import Contact from "./routes/contact";
+import Root, {
+  action as rootAction,
+  loader as rootLoader,
+} from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [{ path: "/contacts/:id", element: <Contact /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
